7550dec0f57b9002bba85405b17035613a139552
Liam-Stannard <www.liamstannard@hotmail.com>
2018-06-18 19:50:22 +0100

    Update do_getprocinf.c

--- do_getprocinf.c
+++ do_getprocinf.c
@@ -5,7 +5,8 @@ DATA:   			09/05/18
 DESCRIPTION:		This is a source file that implements some minix system calls which do the following: select an array of process ids from a specified index in the PM's process table,
 get the name of a process (identified by its pid),
 get information about the children of a given process (identified by its pid)
- */
+ACHIEVED: 9.25/10
+*/
  
 #include <stdio.h>
 #include "pm.h"             // for glo.h:  mp, call_nr, who_p etc.
